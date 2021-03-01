5fb4ab4d20e76fcc2d12b9220551bcc8e2c48ab9
Marcelo Arias <mail.marcelo.as@gmail.com>
2021-02-28 21:10:41 -0500

    Comment AgGridReact package

--- src/components/pages/Dashboard.js
+++ src/components/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { AgGridReact } from 'ag-grid-react';
+// import { AgGridReact } from 'ag-grid-react';
 import 'ag-grid-community/dist/styles/ag-grid.css';
 import 'ag-grid-community/dist/styles/ag-theme-balham.css';
 
