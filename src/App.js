0e0f6c98352a3e1405463f1f61a6f0fa03939816
Marcelo Arias <mail.marcelo.as@gmail.com>
2021-03-02 07:31:37 -0500

    Add Footer component to the Router

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Home from './components/pages/Home';
 import Login from './components/pages/Login';
 import Dashboard from './components/pages/Dashboard';
+import Footer from './components/pages/Footer';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         <Route exact path="/login" component={Login} />
         <Route exact path="/dashboard" component={Dashboard} />
       </Switch>
+      <Footer />
     </BrowserRouter>
   );
 }
